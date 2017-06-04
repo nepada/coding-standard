4337821876d7c0bf9a967666786b28895b2e8a36
Petr Morávek <petr@pada.cz>
2017-06-04 22:24:02 +0200

    Refactor PHPStan config

--- tests/phpstan-bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * This file is part of the nepada/coding-standard.
- * Copyright (c) 2017 Petr Morávek (petr@pada.cz)
- */
-
-require_once __DIR__ . '/../vendor/squizlabs/php_codesniffer/CodeSniffer/Tokens.php';
